be53f9cea9108d09f5c8707782a4c59814685351
Phani Kumar <PhaniKumar@users.noreply.github.com>
2019-04-18 19:59:58 +0530

    FriendlyChat - Subclassing

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    new MaterialApp(
+  runApp(new FriendlyChatApp());
+}
+
+class FriendlyChatApp extends StatelessWidget{
+
+  Widget build(BuildContext context){
+    return new MaterialApp(
       title: "FriendlyChat",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("FriendlyChat"),
-        ),
-      ),
-    ),
-  );
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("FriendlyChat")),
+    );
+  }
 }
