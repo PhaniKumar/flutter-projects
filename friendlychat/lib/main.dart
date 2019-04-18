d29c73dc859b22eddef1774b64e551dd26d58179
Phani Kumar <PhaniKumar@users.noreply.github.com>
2019-04-18 21:07:37 +0530

    FriendlyChat - Adding UI for composing messages

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -14,11 +14,52 @@ class FriendlyChatApp extends StatelessWidget{
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
 
-  Widget build(BuildContext context){
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen>{
+
+  final TextEditingController _textEditingController = new TextEditingController();
+
+  Widget _buildTextComposer(){
+    return new IconTheme(
+        data: new IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: new Row(
+              children: <Widget>[
+                new Flexible(child: new TextField(
+                  controller: _textEditingController,
+                  onSubmitted: _handleSubmitted,
+                  decoration: new InputDecoration.collapsed(hintText: "Send a message"),
+                ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                  child: new IconButton(
+                      icon: new Icon(Icons.send),
+                      onPressed: () => _handleSubmitted(_textEditingController.text)),
+                )
+
+              ],
+            )
+
+        ),
+    );
+  }
+
+  void _handleSubmitted(String text){
+    _textEditingController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("FriendlyChat")),
+      body: _buildTextComposer(),
     );
   }
 }
